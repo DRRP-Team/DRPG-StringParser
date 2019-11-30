18c790299c954ddf894c0308accd3aafd234b33c
PROPHESSOR <xenteze@gmail.com>
2019-11-30 16:19:08 +0200

    Добавлен скрипт для генерации LANGUAGE.CSV

---
+++ generatelanguage.csv.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+
+// const languages = fs.readdirSync(process.argv[2] || '.').filter(x => x.toUpperCase().includes('LANGUAGE'));
+
+const PRIMARY = 'LANGUAGE.ENU';
+const LOCALS = [
+    {
+        file: 'LANGUAGE.RUS.UTF', // Преобразуйте сначала CP1251 в UTF-8
+        lang: 'ru',
+        // charset: 'cp1251'
+    }
+];
+
+function main() {
+    const output = {}; // key: { primary, «lang...» }
+
+    const primary = parseLanguageFile(PRIMARY);
+
+    for (const id in primary) {
+        output[id] = { primary: primary[id], locals: {} };
+    }
+
+    for (const locale of LOCALS) {
+        const localFile = parseLanguageFile(locale.file);
+
+        for (const id in localFile) {
+            output[id].locals[locale.lang] = localFile[id];
+        }
+    }
+
+    const sorted_languages = LOCALS.map(x => x.lang);
+
+    let language_csv = generateCsvRow('default', 'Identifier', 'Remarks', 'Filter', ...sorted_languages);
+
+    for (const id in output) {
+        const string = output[id];
+
+        language_csv += generateCsvRow(string.primary, id, '', '', ...sorted_languages.map(l => string.locals[l] || string.primary));
+    }
+
+    fs.writeFileSync('LANGUAGE.CSV', language_csv, 'utf-8');
+
+    console.info('OK!');
+
+    return output;
+}
+
+/** Парсит содержимое LANGUAGE файла в объёкт формата key: value
+ * @param  {String} data='' - Содержимое LANGUAGE файла
+ * @returns {Object}
+ */
+function parseLanguage(data = '') {
+    const REGEX = /\s*([A-Z_0-9]+)\s*=\s*"(.*)";/g;
+    
+    const output = {};
+
+    let instance = null;
+
+
+    while (instance = REGEX.exec(data)) {
+        const [_, key, value] = instance;
+
+        output[key] = value;//`"${value}"`;
+    }
+
+    return output;
+}
+
+/** Парсит LANGUAGE файл в объёкт формата key: value
+ * @param  {String} file - Путь к LANGUAGE файлу
+ * @returns {Object}
+ */
+function parseLanguageFile(file) {
+    return parseLanguage(fs.readFileSync(file, 'utf-8'));
+}
+
+function generateCsvRow(...row) {
+    return row.map(x => JSON.stringify(x)).join(',') + '\n';
+}
+
+main();
